6bb848a3e82bbf3d83783c5b0a9f3827b473b2f8
Mark Schuurmans <marksmeel@gmail.com>
2026-04-30 11:10:21 +0200

    chore: removed nuxt/vue2

--- packages/nuxt/src/runtime/plugin.vue3.ts
+++
@@ -1,9 +0,0 @@
-import { createORM } from 'pinia-orm'
-import { setActivePinia } from 'pinia'
-import { defineNuxtPlugin } from '#imports'
-import { ormOptions } from '#build/orm-options'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.$pinia.use(createORM(ormOptions))
-  setActivePinia(nuxtApp.$pinia)
-})
