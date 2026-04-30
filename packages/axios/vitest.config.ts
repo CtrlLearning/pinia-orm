a4ad8f4f469d4c964819a0af9e1b2712a96b350b
Mark Schuurmans <marksmeel@gmail.com>
2026-04-30 10:58:19 +0200

    fix: tests falen, warning opgelost

--- packages/axios/vitest.config.ts
+++ packages/axios/vitest.config.ts
@@ -1,6 +1,13 @@
+import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'pinia-orm': path.resolve(__dirname, '../pinia-orm/src/index.ts'),
+      '@pinia-orm/normalizr': path.resolve(__dirname, '../normalizr/src/index.js'),
+    },
+  },
   test: {
     setupFiles: ['./test/setup.ts'],
     externals: ['pinia-orm'],
